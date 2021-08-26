ba88ff31505e059d1395a17a4625c31aa5021965
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-08-26 08:26:56 +0530

    Add files via upload

---
+++ install4.sh
@@ -0,0 +1,6 @@
+sudo apt install python3-pyaudio python3-tk -y
+sudo pip3 install gtts speechrecognition mutagen pygame
+sudo apt install  flac mpg321 -y
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+
+sudo chmod a+rx /usr/local/bin/youtube-dl
